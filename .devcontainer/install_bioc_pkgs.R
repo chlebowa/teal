6ab9f4fda8ba22ffff8421fb17c4e053cf3688fa
Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>
2024-05-13 12:08:00 +0200

    update install_bioc_pkgs.R
    Signed-off-by: Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>

--- .devcontainer/install_bioc_pkgs.R
+++ .devcontainer/install_bioc_pkgs.R
@@ -1,1 +1,55 @@
+#!/usr/bin/env Rscript
 
+# Script that installs BioC packages for a given
+# Takes in the distribution as the first argument.
+args <- commandArgs(trailing = TRUE)
+distribution <- args[1]
+
+# Shared BioC packages to install
+shared_pkgs <- c(
+  "Biobase",
+  "BiocCheck",
+  "BiocGenerics",
+  "BiocStyle",
+  "Bioconductor/BiocBaseUtils",
+  "ComplexHeatmap",
+  "DESeq2",
+  "GenomicRanges",
+  "Gviz",
+  "MultiAssayExperiment",
+  "Rhtslib",
+  "S4Vectors",
+  "SummarizedExperiment",
+  "biomaRt",
+  "edgeR",
+  "limma",
+  "hermes"
+)
+
+# Per distro BioC packages to install
+bioc_pkgs <- list(
+  rstudio = shared_pkgs,
+  `rstudio-local` = shared_pkgs,
+  `debian-clang-devel` = shared_pkgs,
+  `debian-gcc-devel` = shared_pkgs,
+  `fedora-clang-devel` = shared_pkgs,
+  `fedora-gcc-devel` = shared_pkgs,
+  `debian-gcc-patched` = shared_pkgs,
+  `debian-gcc-release` = shared_pkgs
+)
+
+# Get diff of installed and uninstalled packages for
+# idempotent package installation
+new_pkgs <- bioc_pkgs[[distribution]][
+  !(bioc_pkgs[[distribution]] %in% installed.packages()[, "Package"])
+]
+
+# Install only uninstalled packages
+if (length(new_pkgs)) {
+  BiocManager::install(new_pkgs,
+    Ncpus = parallel::detectCores(),
+    force = TRUE,
+    ask = FALSE,
+    update = FALSE
+  )
+}
