4af7e5246ffe184a290d6436246d344906c6e52f
Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>
2024-05-13 12:07:02 +0200

    create install_gh_pkgs.R
    Signed-off-by: Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>

---
+++ .devcontainer/install_gh_pkgs.R
@@ -0,0 +1,40 @@
+#!/usr/bin/env Rscript
+
+# Script that installs additional packages from Github
+# Takes in the distribution as the first argument.
+args <- commandArgs(trailing = TRUE)
+distribution <- args[1]
+
+# Packages to install
+# Regular CRAN packages to install
+shared_pkgs <- c(
+  "insightsengineering/nesttemplate",
+  "openpharma/staged.dependencies@*release",
+  "openpharma/roxylint",
+  "openpharma/roxytypes"
+)
+
+gh_pkgs <- list(
+  rstudio = shared_pkgs,
+  `rstudio-local` = shared_pkgs,
+  `debian-clang-devel` = shared_pkgs,
+  `debian-gcc-devel` = shared_pkgs,
+  `fedora-clang-devel` = shared_pkgs,
+  `fedora-gcc-devel` = shared_pkgs,
+  `debian-gcc-patched` = shared_pkgs,
+  `debian-gcc-release` = shared_pkgs
+)
+
+# Get diff of installed and uninstalled packages for
+# idempotent package installation
+new_pkgs <- gh_pkgs[[distribution]][
+  !(gh_pkgs[[distribution]] %in% installed.packages()[, "Package"])
+]
+
+# Install only uninstalled packages
+if (length(new_pkgs)) {
+  devtools::install_github(
+    new_pkgs,
+    upgrade = "never"
+  )
+}
