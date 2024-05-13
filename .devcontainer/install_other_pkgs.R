c8627c85f14168b69a29396eff9d8d7246a363b7
Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>
2024-05-13 12:06:42 +0200

    create install_other_pkgs.R
    Signed-off-by: Aleksander Chlebowski <114988527+chlebowa@users.noreply.github.com>

---
+++ .devcontainer/install_other_pkgs.R
@@ -0,0 +1,65 @@
+#!/usr/bin/env Rscript
+
+# Script that installs R packages from alternate sources.
+# Takes in the distribution as the first argument.
+args <- commandArgs(trailing = TRUE)
+distribution <- args[1]
+
+# Pharmaverse packages to be installed
+pharmaverse_packages <- c(
+  "scda",
+  "scda.2022",
+  "goshawk",
+  "teal.goshawk",
+  "osprey",
+  "teal.osprey",
+  "tern.mmrm",
+  "teal.modules.hermes",
+  "tern.rbmi"
+)
+
+# Statistics packages
+stat_pkgs <- c(
+  "cmdstanr"
+)
+
+# List for packages to be installed in a given distribution
+other_pkgs <- list(
+  rstudio = c(
+    stat_pkgs
+  ),
+  `rstudio-local` = c(
+    pharmaverse_packages,
+    stat_pkgs
+  ),
+  `debian-clang-devel` = c(stat_pkgs),
+  `debian-gcc-devel` = c(stat_pkgs),
+  `fedora-clang-devel` = c(stat_pkgs),
+  `fedora-gcc-devel` = c(stat_pkgs),
+  `debian-gcc-patched` = c(stat_pkgs),
+  `debian-gcc-release` = c(stat_pkgs)
+)
+
+# Get diff of installed and uninstalled packages for
+# idempotent package installation
+new_pkgs <- other_pkgs[[distribution]][
+  !(other_pkgs[[distribution]] %in% installed.packages()[, "Package"])
+]
+
+# Install only uninstalled packages
+if (length(new_pkgs)) {
+  install.packages(
+    new_pkgs,
+    repos = c(
+      "https://insightsengineering.r-universe.dev",
+      "https://cloud.r-project.org/",
+      "https://mc-stan.org/r-packages/"
+    ),
+    Ncpus = parallel::detectCores(),
+    ask = FALSE,
+    upgrade = "never"
+  )
+}
+
+# Install cmdstan
+cmdstanr::install_cmdstan(cores = parallel::detectCores())
